48953d8dbf84edc242b7e5389d8e048acc08390f
lynnlangit <lynnlangit@live.com>
2016-07-20 08:15:16 -0700

    cleaning Yellowfin EC2 script

--- D3-AWSDataWarehouse/3_YellowFinSetup.sh
+++ D3-AWSDataWarehouse/3_YellowFinSetup.sh
@@ -2,36 +2,28 @@
 
 set -e
 
-#------Sets up YellowFin from AWS Marketplace on AWS EC2)------------
+#------Sets up YellowFin from AWS Marketplace on AWS EC2------------
 
 REGION = 'ap-southeast-2'
-MYKEYPAIR = ''
-
+MYKEYPAIR = '< my key >' #use a profile, not a key!
 VPCID=`aws ec2 describe-vpc --region $REGION`
 SUBNETID=`aws ec2 describe-subnet --region $REGION`
-ROUTETABLEID=`aws ec2 describe-route-tables`
-aws ec2 create-route --route-table-id $routetableId --destination-cidr-block 0.0.0.0/0 --gateway-id $gatewayid
-
-# Add an Elastic IP
-echo ipAddress=`aws ec2 allocate-address --domain vpc | jq -r .PublicIp`
-echo $ipAddress
-echo $allocation-id
+ROUTETABLEID=`aws ec2 describe-route-tables --region $REGION`
+GATEWAYID='aws ec2 describe-gateways --region $REGION'
+# Add an Elastic IP and TODO - verify syntax
+IPADDRESS=`aws ec2 allocate-address --domain vpc`
+aws ec2 create-route --route-table-id $ROUTETABLEID --destination-cidr-block 0.0.0.0/0 --gateway-id $GATEWAYID
+# Add YellowFin rule(s) into the security group and TODO - needs rule for SSH (20), HTTP/S (80/443)
+SECURITYGROUPID=`aws ec2 describe-security-groups --filters Name=vpc-id,Values=$VPCID`
+aws ec2 authorize-security-group-ingress --group-id $SECURITYGROUPID  --protocol tcp --port 5439 --cidr 10.0.0.0/16
 
 # Launch YelloFin EC2 Instance from the AWS Marketplace using AMI for Australia
 # Use the AMI from your region for YelloFin from the marketplace 
-echo instance-id=`aws ec2 run-instances --image-id ami-209ebd43 --count 1 --instance-type m3.large \
-    --key-name MyKeyPair --security-group-ids $securityGroupId --subnet-id $subnetid`
-echo $instance-id
-
-# Attach the Elastic IP to the YelloFin  Instance
-# TOVERIFY
-aws ec2 associate-address --instance-id $instance-id --allocation-id $allocation-id
-
-# Add YellowFin rule(s) into the security group
-# TODO - needs rule for SSH (20), HTTP/S (80/443)
-securityGroupId=`aws ec2 describe-security-groups --filters Name=vpc-id,Values=$vpcId`
-aws ec2 authorize-security-group-ingress --group-id $securityGroupId  --protocol tcp --port 5439 --cidr 10.0.0.0/16
-
+# TODO - what is the allocation id?
+AMIYELLOWFIN = 'ami-209ebd43'  # this is for Australia region
+INSTANCEID=`aws ec2 run-instances --image-id $AMIYELLOWFIN --count 1 --instance-type m3.large \
+    --key-name $MYKEYPAIR --security-group-ids $SECURITYGROUPID --subnet-id $SUBNETID`
+aws ec2 associate-address --instance-id $INSTANCEID --allocation-id $allocation-id
 # TODO - Pattern to add tags to resources
 aws ec2 create-tags --resources ami-<value> i-<value> --tags Key=show,Value=ndc
 
